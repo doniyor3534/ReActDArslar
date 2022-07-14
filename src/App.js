2e7af86849413b37008ae3abea4c5814d237876a
doniyor3534 <doniyortursunov800@gmail.com>
2022-07-14 10:55:47 +0500

    ff222222222222

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import React  from 'react';
+import Useeffect from './Useeffect/Useeffect'
+
+
+function App() {
+  
+  return (
+    <div className='app'>
+         <Useeffect/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Reducer/IndexReduser2.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useState } from 'react';
+import { useReducer } from 'react';
+import { initialstate2, Reduser2 } from './Reduser2';
+
+const IndexReduser2 = () => {
+    const [state,dispatch]=useReducer(Reduser2,initialstate2)
+    const {data2}= state ;
+    const [input,setInput]=useState('')
+    const send=(e)=>{
+        e.preventDefault()
+        dispatch({type:'add',payload:{id:new Date().getTime(),name:input}})
+    }
+    const del=(id)=>{
+        dispatch({type:'delete',payload:id})
+    }
+    return (
+        <div>
+            <form onSubmit={send}>
+                <input type="text"  value={input} onChange={(e)=>setInput(e.target.value)}/>
+                <button type='submit'>Add</button>
+            </form>
+            <div className="cards">
+                {
+                    data2.map((val)=>(
+                        <div className="card" key={val.id}>
+                            <h1>{val.name}</h1>
+                            <button className='btn' onClick={()=>del(val.id)}>delete</button>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    );
+};
+
+export default IndexReduser2;
